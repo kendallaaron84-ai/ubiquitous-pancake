ec840e0b78a7555c1a6faf025660849420599d0f
Kendall Aaron <kendallaaron84@gmail.com>
2026-07-04 15:49:09 -0500

    anayltics page

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,6 +1,8 @@
 import Layout from "@/components/layout"
 import Analytics from "@/components/section/analytics"
 
+export const dynamic = 'force-dynamic';
+
 export default function AnalyticsPage() {
 	return (
 		<Layout >
